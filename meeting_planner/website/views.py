c5b09238853322beaa1afbb4efda16a94ee83799
Lance Johnson <rlancejohnson@gmail.com>
2022-01-10 12:14:42 -0700

    Pass message variable to welcome template

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html", {
+        "message": "This data was sent from the view to the template"
+    })
 
 def date(request):
     return HttpResponse(f"This page was served at {str(datetime.now())}")
