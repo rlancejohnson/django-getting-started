7e965867444ad749f6b57cdbe62b8c0bc8a6f66e
Lance Johnson <rlancejohnson@gmail.com>
2022-01-10 13:13:55 -0700

    Add meeting links to welcome page and home link back to welcome page

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -20,8 +20,8 @@ from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome),
+    path('', welcome, name='welcome'),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', detail),
+    path('meetings/<int:id>', detail, name='detail'),
 ]
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -8,7 +8,7 @@ from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
     return render(request, "website/welcome.html", {
-        "num_meetings": Meeting.objects.count()
+        "meetings": Meeting.objects.all()
     })
 
 def date(request):
