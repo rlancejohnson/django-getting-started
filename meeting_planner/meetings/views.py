b44be930361e72c70ce4845ec56e4ba751d2cc53
Lance Johnson <rlancejohnson@gmail.com>
2022-01-10 17:34:25 -0700

    Add new meeting form

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 
@@ -15,4 +16,21 @@ def rooms(request):
 
     return render(request, "meetings/rooms.html", {
         "rooms": rooms
+    })
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == 'POST':
+        # form has been submitted, process data
+        form = MeetingForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('welcome')
+    else:
+        form = MeetingForm()
+
+    return render(request, "meetings/new.html", {
+        "form": form
     })
